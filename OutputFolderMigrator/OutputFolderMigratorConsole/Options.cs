c54dc1f79c710cb148162914886f6ef4388d1a44
Wesley Kenis <wesley.kenis@telenet.be>
2016-04-26 09:25:27 +0200

    Initial commit

---
+++ OutputFolderMigrator/OutputFolderMigratorConsole/Options.cs
@@ -0,0 +1,40 @@
+﻿#region References
+
+using System;
+using CommandLine;
+using CommandLine.Text;
+
+#endregion
+
+namespace OutputFolderMigratorConsole
+{
+	internal enum ProgrammingLanguage
+	{
+		VisualBasic,
+		CSharp,
+	}
+
+	internal class Options
+	{
+		[Option('s', nameof(SourceDirectory), Required = false, HelpText = "Source directory to process")]
+		public string SourceDirectory { get; set; }
+
+		[Option('o', nameof(OutputDirectory), DefaultValue = @"bin\$(Configuration)\$(Platform)\",
+			HelpText =
+				"Directory to set as outputdirectory. Valid options are:\n- an absolute or relative path\n - a combination of MSBuild parameters like the default bin\\$(Configuration)\\$(Platform)\\."
+			)]
+		public string OutputDirectory { get; set; }
+
+		[Option('l', nameof(Language), DefaultValue = ProgrammingLanguage.CSharp, HelpText = "The language for which projects are processed")]
+		public ProgrammingLanguage Language { get; set; }
+
+		[ParserState]
+		public IParserState LastParserState { get; set; }
+
+		[HelpOption]
+		public string GetUsage()
+		{
+			return HelpText.AutoBuild(this, (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+		}
+	}
+}
---
+++ OutputFolderMigrator/OutputFolderMigratorConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿#region References
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Xml.Linq;
+using CommandLine;
+
+#endregion
+
+namespace OutputFolderMigratorConsole
+{
+	class Program
+	{
+		private const string xmlns = "http://schemas.microsoft.com/developer/msbuild/2003";
+
+		static void Main(string[] args)
+		{
+			Options options = new Options();
+			if (!Parser.Default.ParseArguments(args, options)) return;
+			if (string.IsNullOrWhiteSpace(options.SourceDirectory)) options.SourceDirectory = Environment.CurrentDirectory;
+			foreach (
+				string projectFile in Directory.EnumerateFiles(options.SourceDirectory, GetExtention(options.Language), SearchOption.AllDirectories))
+			{
+				ProcessProjectFile(projectFile, options.OutputDirectory);
+			}
+		}
+
+		private static string GetExtention(ProgrammingLanguage language)
+		{
+			switch (language)
+			{
+				case ProgrammingLanguage.CSharp:
+					return "*.csproj";
+				case ProgrammingLanguage.VisualBasic:
+					return "*.vbproj";
+			}
+			throw new InvalidEnumArgumentException();
+		}
+
+		private static void ProcessProjectFile(string projectFile, string outputDirectory)
+		{
+			XDocument doc = XDocument.Load(projectFile);
+
+			IEnumerable<XElement> paths = doc.Descendants(XName.Get("OutputPath", xmlns));
+			foreach (XElement path in paths)
+			{
+				path.Value = outputDirectory;
+			}
+			doc.Save(projectFile);
+		}
+	}
+}
